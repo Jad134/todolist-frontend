be00cc1a40a19641102a72eafd1a3245c2a75b45
Jad El Nader <Jadelnader99@gmail.com>
2024-06-26 11:54:03 +0200

    create authentication at angular from BA

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  email: string = ''
+  constructor() {}
+  username: string = ''
   password: string = ''
+  authService = inject(AuthService)
 
-  login() {
-    console.log('geht');
+  async login() {
+    try {
+      let resp = await this.authService.loginWithUsernameAndPassword(this.username, this.password)
+     console.log(resp);
+     
+    }
+    catch (e) {
+      console.error('Fehler bei der Anmeldung', e);
 
+    }
   }
+
+
+
+
 }
+
+
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+
+  async loginWithUsernameAndPassword(username: string, password: string) {
+    const url = environment.baseUrl + "/login/";
+    const body = {
+      "username": username,
+      "password": password
+    }
+    return lastValueFrom(this.http.post(url, body)) 
+  }
+
+}
